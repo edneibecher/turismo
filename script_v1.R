9b6d7123d83d007e2a3f69e5828ee7657cf38a76
Ednei Becher <edneibecher@gmail.com>
2020-08-14 14:46:04 -0300

    carregando o primeiro arquivo

--- script_v1.R
+++ script_v1.R
@@ -1,1 +1,8 @@
 # teste de uso do GitHub
+
+# para carregar arquivos
+library(readr)
+OrganizadoraDeEventos201904TrimestreCadasturPJ <- read_delim("OrganizadoraDeEventos201904TrimestreCadasturPJ.csv", 
+                                                               +     ";", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
+                                                               +     trim_ws = TRUE)
+
