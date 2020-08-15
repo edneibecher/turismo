399da063d6e3e73f7d229488db7976f282689a05
Ednei Becher <edneibecher@gmail.com>
2020-08-14 23:45:46 -0300

    Primeiras análises

--- script_v1.R
+++ script_v1.R
@@ -6,3 +6,30 @@ OrganizadoraDeEventos201904TrimestreCadasturPJ <- read_delim("OrganizadoraDeEven
                                                                +     ";", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
                                                                +     trim_ws = TRUE)
 
+# filtrando por estado e por localicade e gerando um grafico de colunas
+teste %>% 
+  filter(UF=="RS") %>% 
+  group_by(Localidade) %>% 
+  ggplot(aes(Localidade))+geom_bar()+
+  theme(axis.text.x=element_text(size=rel(1.1), angle=90))
+
+# contando o numero de empresas por localidade
+teste %>% 
+  filter(UF=="RS") %>% 
+  count(Localidade) 
+
+# calcular o tempo de funcionamento em anos
+library(lubridate)
+
+teste$`Data de Inicio da Operação`<- dmy(teste$`Data de Inicio da Operação`)
+
+as.duration(today()-teste$`Data de Inicio da Operação`) # ou também
+
+year(today())-year(teste$`Data de Inicio da Operação`)
+
+# para obter a data atual
+
+Sys.Date() # ou
+today()
+
+
